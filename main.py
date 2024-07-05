4b7ca63647b51f53ec29cc46c3d639a8624763d3
Mahda Noura <mahdanoura@gmail.com>
2024-07-05 23:26:51 +0200

    Linkml model seperations (#15)
    * hctl schema seperated from TD and improved definitions
    
    * hctl concepts removed from TD schema
    
    * minor changes
    
    * wotsec schema seperated from TD and completed class definitions
    
    * wotsec terms removed from TD
    
    * update of terms
    
    * seperated from thing description schema, fixed doc generation problem
    
    * seperated from thing description schema, fixed doc generation problem
    
    * fixed doc generation problem
    
    * removed the config file, and added the generation to one complete LinkML file for all the smaller LinkML files
    
    * readme for the schemas added
    
    * updates on the schema, and mainly fixed profile combination of array of uris or a uri
    
    * fixed security and securityDefinition classes bugs
    
    * modified SecurityScheme class to reuse slots from the td schema
    
    * further refractoring of the schema according to the spec
    
    * seperated json schema class
    
    * defined the dataschema class
    
    * minor updates of refinement
    
    * completed the schema definition
    
    * syntax issue fixed
    
    * aliases added
    
    * refractoring of the schema
    
    * refractoring of the schema

--- main.py
+++ main.py
@@ -1,105 +1,71 @@
 import argparse
 import subprocess
-import yaml
 
 from linkml.generators.jsonschemagen import JsonSchemaGenerator
 from linkml.generators.shaclgen import ShaclGenerator
-from linkml.generators.shexgen import ShExGenerator
 from linkml.generators.owlgen import OwlSchemaGenerator
-from linkml.generators.markdowngen import MarkdownGenerator
-from linkml.generators.jsonldgen import JSONLDGenerator
 from linkml.generators.jsonldcontextgen import ContextGenerator
-from linkml.generators.typescriptgen import TypescriptGenerator
-from linkml.generators.protogen import ProtoGenerator
 from linkml.generators.docgen import DocGenerator
+from linkml.generators.linkmlgen import LinkmlGenerator
 from pathlib import Path
 
 RESOURCES_PATH = Path('resources')
 GENS_PATH = RESOURCES_PATH / 'gens'
-JSON_SCHEMA_PATH = GENS_PATH / 'jsonschema'
-YAML_SCHEMA_PATH = RESOURCES_PATH / 'thing_description_schema.yaml'
-SOURCE_PATH = Path('src')
-LINKML_PATH = Path('linkml')
-LINKML_GENERATORS_CONFIG_YAML_PATH = SOURCE_PATH / LINKML_PATH / 'config.yaml'
-DOCDIR = GENS_PATH / 'docs'
-
+SCHEMA_PATH = RESOURCES_PATH / 'schemas'
+YAML_SCHEMA_PATH = SCHEMA_PATH / 'thing_description.yaml'
+DOCDIR = GENS_PATH / 'docs' / 'ontology'
+GENERATORS = ['jsonschema', 'shacl', 'jsonldcontext', 'linkml']
 
 def serve_docs():
     subprocess.run(['mkdocs', 'serve'], check=True)
 
 
-def generate_docs(yaml_path, docdir):
-    docdir.mkdir(parents=True, exist_ok=True)
-    doc_generator = DocGenerator(yaml_path)
-    doc_generator.serialize(directory=str(docdir))
+def generate_docs():
+    DOCDIR.mkdir(parents=True, exist_ok=True)
+    doc_generator = DocGenerator(YAML_SCHEMA_PATH, mergeimports=False)
+    doc_generator.serialize(directory=str(DOCDIR))
 
-
-def config_file_parse(config_path):
-    with open(config_path, 'r') as config_file:
-        config_content = config_file.read()
-    yaml_content = yaml.safe_load(config_content)
-    generator_args = yaml_content.get('generator_args', {})
-    generators = list(generator_args.keys())
-    return generators
-
-
-def main(yaml_path, config_path, generate_docs_flag, serve_docs_flag):
-    yaml_content = yaml_path.read_text()
-    generators = config_file_parse(config_path)
-
-    for generator in generators:
+def main(generate_docs_flag, serve_docs_flag):
+    if not YAML_SCHEMA_PATH.exists():
+        print(f"LinkML schema file does not exist: {YAML_SCHEMA_PATH}")
+        return
+    for generator in GENERATORS:
         output_dir = GENS_PATH / generator
         output_dir.mkdir(parents=True, exist_ok=True)
-
         if generator == 'jsonschema':
-            json_schema_generator = JsonSchemaGenerator(yaml_content, top_class="Thing")
-            json_schema_generator.root_class = "Thing"
-            (JSON_SCHEMA_PATH / f'{yaml_path.stem}.json').write_text(json_schema_generator.serialize())
-        elif generator == 'owl':
-            owl_generator = OwlSchemaGenerator(yaml_content)
-            (output_dir / f'{yaml_path.stem}.owl.ttl').write_text(owl_generator.serialize())
-        elif generator == 'markdown':
-            markdown_generator = MarkdownGenerator(yaml_content)
-            (output_dir / f'{yaml_path.stem}.md').write_text(markdown_generator.serialize(directory=str(output_dir)))
+            # json_schema_generator = JsonSchemaGenerator(yaml_content, top_class="Thing")
+            json_schema_generator = JsonSchemaGenerator(YAML_SCHEMA_PATH, mergeimports=True)
+            (output_dir / 'jsonschema.json').write_text(json_schema_generator.serialize())
         elif generator == 'shacl':
-            shacl_generator = ShaclGenerator(yaml_content)
-            (output_dir / f'{yaml_path.stem}.shacl.ttl').write_text(shacl_generator.serialize())
-        elif generator == 'shex':
-            shex_generator = ShExGenerator(yaml_content)
-            (output_dir / f'{yaml_path.stem}.shex').write_text(shex_generator.serialize())
-        elif generator == 'jsonld':
-            jsonld_generator = JSONLDGenerator(yaml_content)
-            (output_dir / f'{yaml_path.stem}.jsonld').write_text(jsonld_generator.serialize())
+            shacl_generator = ShaclGenerator(YAML_SCHEMA_PATH, mergeimports=False, closed=True, suffix='Shape')
+            (output_dir / 'shapes.shacl.ttl').write_text(shacl_generator.serialize())
+        elif generator == 'owl':
+            owl_generator = OwlSchemaGenerator(YAML_SCHEMA_PATH,)
+            (output_dir / 'ontology.owl.ttl').write_text(owl_generator.serialize())
         elif generator == 'jsonldcontext':
-            context_generator = ContextGenerator(yaml_content)
-            (output_dir / f'{yaml_path.stem}.context.jsonld').write_text(context_generator.serialize())
-        elif generator == 'typescript':
-            typescript_generator = TypescriptGenerator(yaml_content)
-            (output_dir / f'{yaml_path.stem}.js').write_text(typescript_generator.serialize())
-        elif generator == 'protobuf':
-            protobuf_generator = ProtoGenerator(yaml_content)
-            (output_dir / f'{yaml_path.stem}.js').write_text(protobuf_generator.serialize())
+            context_generator = ContextGenerator(YAML_SCHEMA_PATH, mergeimports=False)
+            (output_dir / 'context.jsonld').write_text(context_generator.serialize())
+        elif generator == 'linkml':
+            linkml_generator = LinkmlGenerator(YAML_SCHEMA_PATH, mergeimports=True, format='yaml', output='linkml.yaml')
+            (output_dir / 'linkml.yaml').write_text(linkml_generator.serialize())
         else:
             print(f"Unknown generator: {generator}")
             continue
 
     if generate_docs_flag:
-        generate_docs(yaml_path, DOCDIR)
+        generate_docs()
 
     if serve_docs_flag:
-        generate_docs(yaml_path, DOCDIR)
+        generate_docs()
         serve_docs()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='WoT TD toolchain for automating specification generation')
-    parser.add_argument('-y', '--yaml', default=YAML_SCHEMA_PATH,
-                        help='Path to the LinkML schema formatted as a YAML file.')
-    parser.add_argument('-c', '--config-file', default=LINKML_GENERATORS_CONFIG_YAML_PATH,
-                        help='Path to YAML configuration for specifying the required LinkML generators.')
     parser.add_argument('-l', '--local-docs', action='store_true',
                         help='Boolean for local documentation generation.')
+
     parser.add_argument('-s', '--serve-docs', action='store_true',
                         help='Boolean for serving the generated documentation.')
     args = parser.parse_args()
-    main(Path(args.yaml), Path(args.config_file), args.local_docs, args.serve_docs)
+    main(args.local_docs, args.serve_docs)
