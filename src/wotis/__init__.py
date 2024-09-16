f307f2c19e348925af18612f0b461312f23c99a7
mahdanoura <mahda.noura@siemens.com>
2024-09-16 21:08:47 +0200

    package update

--- src/wotis/__init__.py
+++ src/wotis/__init__.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-
 RESOURCES_PATH = Path('resources')
 GENS_PATH = RESOURCES_PATH / 'gens'
 SCHEMA_PATH = RESOURCES_PATH / 'schemas'
--- src/wotis/cli.py
+++ src/wotis/cli.py
@@ -1,5 +1,4 @@
 """Command line interface for WoTIS."""
-
 import click
 import logging
 from pathlib import Path
@@ -15,6 +14,7 @@ from linkml.generators.linkmlgen import LinkmlGenerator
 from linkml_runtime.utils.schemaview import SchemaView
 
 from src.wotis import YAML_SCHEMA_PATH, GENS_PATH, GENERATORS, DOCDIR
+from src.wotis.post_processors.jsonld_context_postprocessor import post_process_jsonld_context
 
 
 input_option = click.option('-i', '--input_schema',
@@ -38,7 +38,6 @@ serve_docs_option = click.option('-s',
                                  help="Boolean for serving the generated documentation.")
 
 
-# Documentation serving function
 def serve_documentation():
     subprocess.run(['mkdocs', 'serve'], check=True)
 
@@ -52,22 +51,24 @@ def generate_documentation():
 # Main generation function
 def run_generator(schema_view, generator, output_dir):
     if generator == 'jsonschema':
-        logging.info(f"JSON Schema generation")
+        logging.info(f"Proceeding with LinkML to JSON Schema convertion")
         json_schema_generator = JsonSchemaGenerator(schema_view.schema, mergeimports=True)
         (output_dir / 'jsonschema.json').write_text(json_schema_generator.serialize())
     elif generator == 'shacl':
+        logging.info(f"Proceeding with LinkML to SHACL convertion")
         shacl_generator = ShaclGenerator(schema_view.schema, mergeimports=False, closed=True, suffix='Shape')
         (output_dir / 'shapes.shacl.ttl').write_text(shacl_generator.serialize())
     elif generator == 'owl':
+        logging.info(f"Proceeding with LinkML to OWL convertion")
         owl_generator = OwlSchemaGenerator(schema_view.schema, )
         (output_dir / 'ontology.owl.ttl').write_text(owl_generator.serialize())
     elif generator == 'jsonldcontext':
+        logging.info(f"Proceeding with LinkML to JSON-LD Context convertion")
         context_generator = ContextGenerator(schema_view.schema, mergeimports=True)
-        # (output_dir / 'context.jsonld').write_text(post_process_jsonld_context(linkml_schema_view,
-        #                                                                        context_generator.serialize()))
+        (output_dir / 'context.jsonld').write_text(post_process_jsonld_context(schema_view,
+                                                                               context_generator.serialize()))
     elif generator == 'linkml':
-        linkml_generator = LinkmlGenerator(schema_view.schema, mergeimports=True, format='yaml',
-                                           output='linkml.yaml')
+        linkml_generator = LinkmlGenerator(schema_view.schema, mergeimports=True, format='yaml', output='linkml.yaml')
         (output_dir / 'linkml.yaml').write_text(linkml_generator.serialize())
     else:
         print(f"Unknown generator: {generator}")
@@ -122,7 +123,7 @@ def generate_wot_resources(input_schema: str, generate_docs: bool, serve_docs: b
             logging.info(f"Generating documentation locally as markdown files...")
             generate_documentation()
         if serve_docs:
-            logging.info(f"GServing documentation...")
+            logging.info(f"Serving documentation...")
             serve_documentation()
 
 
