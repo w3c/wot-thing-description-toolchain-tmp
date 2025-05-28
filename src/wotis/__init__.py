c14758ffb8249873328db701b12611e0f4c9ad15
Mahda Noura <mahdanoura@gmail.com>
2025-05-28 10:29:53 +0200

    JSON-LD Context Validation and Comparison against benchmark (#40)
    * readme update
    
    * readme update
    
    * minor updates
    
    * removal of init file
    
    * Update README.md
    
    Co-authored-by: Luca Barbato <lu_zero@gentoo.org>
    
    * Update README.md
    
    Co-authored-by: Luca Barbato <lu_zero@gentoo.org>
    
    * Update README.md
    
    Co-authored-by: Ted Thibodeau Jr <tthibodeau@openlinksw.com>
    
    * testing of generated context file against benchmark
    
    * Revert "testing of generated context file against benchmark"
    
    This reverts commit 1e893e562879a7ab3fad9209c74ad712315ab16a.
    
    revert of commit
    
    * manual bechmark context file path added
    
    * script for validating generated context file
    
    * manually written context file for testing and benchmarking
    
    * chore: update linkml and linkml-runtime to v1.9.2 for Python 3.13 compatibility
    
    * docs: resolve README.md conflicts and update Python version requirement to 3.13
    
    ---------
    
    Co-authored-by: Luca Barbato <lu_zero@gentoo.org>
    Co-authored-by: Ted Thibodeau Jr <tthibodeau@openlinksw.com>

--- src/wotis/__init__.py
+++ src/wotis/__init__.py
@@ -3,7 +3,9 @@ from pathlib import Path
 RESOURCES_PATH = Path('resources')
 GENS_PATH = RESOURCES_PATH / 'gens'
 SCHEMA_PATH = RESOURCES_PATH / 'schemas'
+MANUAL_PATH = RESOURCES_PATH / 'manuals'
 YAML_SCHEMA_PATH = SCHEMA_PATH / 'thing_description.yaml'
+GENERATED_LINKML_SCHEMA = GENS_PATH / 'linkml/linkml.yaml'
 DOCDIR = GENS_PATH / 'docs' / 'ontology'
 
 GENERATORS = ['jsonschema', 'shacl', 'jsonldcontext', 'linkml']
