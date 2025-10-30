a976e6590f82968b15dbf821fcfd6ee6336a1abc
mahdanoura <mahda.noura@siemens.com>
2025-10-30 09:57:47 +0100

    chore: configuration parameters for the spec generators added

--- src/wotis/__init__.py
+++ src/wotis/__init__.py
@@ -3,9 +3,19 @@ from pathlib import Path
 RESOURCES_PATH = Path('resources')
 GENS_PATH = RESOURCES_PATH / 'gens'
 SCHEMA_PATH = RESOURCES_PATH / 'schemas'
-MANUAL_PATH = RESOURCES_PATH / 'manuals'
+MANUAL_PATH = RESOURCES_PATH / 'benchmark_schemas'
 YAML_SCHEMA_PATH = SCHEMA_PATH / 'thing_description.yaml'
 GENERATED_LINKML_SCHEMA = GENS_PATH / 'linkml/linkml.yaml'
 DOCDIR = GENS_PATH / 'docs' / 'ontology'
 
-GENERATORS = ['jsonschema', 'shacl', 'jsonldcontext', 'linkml']
+GENERATORS = ['jsonschema', 'shacl', 'jsonldcontext', 'owl', 'linkml']
+
+RESPEC_TEMPLATE_PATH = RESOURCES_PATH / 'index.template.html'
+RESPEC_FRAGMENT_PATH = GENS_PATH / 'schema_fragment.html'
+FINAL_SPEC_PATH = GENS_PATH / 'final-spec.html'
+CORE_SCHEMA_PLACEHOLDER = '<!-- LINKML_SCHEMA_PLACEHOLDER -->'
+RDF_DEFINITIONS_PLACEHOLDER_V1 = '<!-- rendered from RDF definitions --> %s'
+
+from .postprocessors.jsonschema_postprocessor import post_process_jsonschema
+
+__all__ = ['post_process_jsonschema']
