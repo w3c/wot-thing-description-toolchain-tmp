32adadcb5d087a600729c79e1d75b30b42e44f80
mahdanoura <mahda.noura@siemens.com>
2025-10-30 09:58:50 +0100

    feat: removed Mkdocs and use respec for the doc generation

--- src/wotis/cli.py
+++ src/wotis/cli.py
@@ -1,10 +1,12 @@
 """Command line interface for WoTIS."""
 import click
+import json
 import logging
-from pathlib import Path
 import subprocess
-
 import yaml
+
+from pathlib import Path
+
 from linkml.generators.jsonschemagen import JsonSchemaGenerator
 from linkml.generators.shaclgen import ShaclGenerator
 from linkml.generators.owlgen import OwlSchemaGenerator
@@ -13,8 +15,14 @@ from linkml.generators.docgen import DocGenerator
 from linkml.generators.linkmlgen import LinkmlGenerator
 from linkml_runtime.utils.schemaview import SchemaView
 
-from src.wotis import YAML_SCHEMA_PATH, GENS_PATH, GENERATORS, DOCDIR
+from src.wotis import (YAML_SCHEMA_PATH, GENS_PATH, GENERATORS,
+                       RESPEC_TEMPLATE_PATH, RESPEC_FRAGMENT_PATH, FINAL_SPEC_PATH,
+                       CORE_SCHEMA_PLACEHOLDER, RDF_DEFINITIONS_PLACEHOLDER_V1)
+
 from src.wotis.postprocessors.jsonld_context_postprocessor import post_process_jsonld_context
+from src.wotis.postprocessors.jsonschema_postprocessor import post_process_jsonschema
+
+CUSTOM_TEMPLATE_DIR = Path(__file__).parent.parent.parent / 'templates'
 
 
 input_option = click.option('-i', '--input_schema',
@@ -22,38 +30,51 @@ input_option = click.option('-i', '--input_schema',
                             show_default=True,
                             help="Path to the input schema specified as LinkML yaml.",
                             default=YAML_SCHEMA_PATH)
-docs_option = click.option('-d',
-                           '--generate_docs',
-                           type=bool,
-                           is_flag=True,
-                           default=False,
-                           show_default=True,
-                           help="Boolean for local documentation generation.")
-serve_docs_option = click.option('-s',
-                                 '--serve_docs',
-                                 type=bool,
-                                 is_flag=True,
-                                 default=False,
-                                 show_default=True,
-                                 help="Boolean for serving the generated documentation.")
+respec_option = click.option('-r',
+                             '--generate_spec',
+                             type=bool,
+                             is_flag=True,
+                             default=False,
+                             show_default=True,
+                             help="Boolean for generating the final Respec HTML specification.")
 
 
 def serve_documentation():
     subprocess.run(['mkdocs', 'serve'], check=True)
 
 
-def generate_documentation():
-    DOCDIR.mkdir(parents=True, exist_ok=True)
-    doc_generator = DocGenerator(YAML_SCHEMA_PATH, mergeimports=False)
-    doc_generator.serialize(directory=str(DOCDIR))
+def assemble_respec_spec(template_path: Path, final_path: Path):
+    """
+    Reads the Respec template and saves it directly to the final path.
+    """
+    logging.info("Assembling final Respec specification (Simple Copy)...")
+
+    if not template_path.exists():
+        logging.error(f"Respec template not found at {template_path}. Cannot assemble spec.")
+        return
+    respec_template = template_path.read_text(encoding='utf-8')
+    final_path.write_text(respec_template, encoding='utf-8')
+    logging.info(f"Final Respec specification saved to {final_path}")
 
 
-# Main generation function
+def generate_respec_spec(input_path: Path, gens_path: Path):
+    """
+    Generates the Respec specification by simply copying the template.
+    We only need the path to the template and the desired final output path.
+    """
+    assemble_respec_spec(RESPEC_TEMPLATE_PATH, FINAL_SPEC_PATH)
+
+
 def run_generator(schema_view, generator, output_dir):
     if generator == 'jsonschema':
         logging.info(f"Proceeding with LinkML to JSON Schema convertion")
         json_schema_generator = JsonSchemaGenerator(schema_view.schema, mergeimports=True)
-        (output_dir / 'jsonschema.json').write_text(json_schema_generator.serialize())
+        processed_content  = json_schema_generator.serialize()
+        #processed_content = post_process_jsonschema(schema_view, raw_content)
+        output_file = output_dir / 'jsonschema.json'
+        with output_file.open('w', encoding='utf-8') as f:
+            json.dump(processed_content, f, indent=2, ensure_ascii=False)
+        logging.info(f"Post-processed JSON Schema saved to {output_file}")
     elif generator == 'shacl':
         logging.info(f"Proceeding with LinkML to SHACL convertion")
         shacl_generator = ShaclGenerator(schema_view.schema, mergeimports=False, closed=True, suffix='Shape')
@@ -65,8 +86,9 @@ def run_generator(schema_view, generator, output_dir):
     elif generator == 'jsonldcontext':
         logging.info(f"Proceeding with LinkML to JSON-LD Context convertion")
         context_generator = ContextGenerator(schema_view.schema, mergeimports=True)
-        (output_dir / 'context.jsonld').write_text(post_process_jsonld_context(schema_view,
-                                                                               context_generator.serialize()))
+        (output_dir / 'context.jsonld').write_text(context_generator.serialize())
+        # (output_dir / 'context.jsonld').write_text(post_process_jsonld_context(schema_view,
+        #                                                                        context_generator.serialize()))
     elif generator == 'linkml':
         linkml_generator = LinkmlGenerator(schema_view.schema, mergeimports=True, format='yaml', output='linkml.yaml')
         (output_dir / 'linkml.yaml').write_text(linkml_generator.serialize())
@@ -97,34 +119,31 @@ def main(verbose: int, quiet: bool):
 
 @main.command()
 @input_option
-@docs_option
-@serve_docs_option
-def generate_wot_resources(input_schema: str, generate_docs: bool, serve_docs: bool):
+@respec_option
+def generate_wot_resources(input_schema: str, generate_spec: bool):
     """
-    Generating WoT resources (RDF, JSON-LD Context, SHACL Shapes, and JSON Schema) from manually constructed
-    LinkML-based schemas.
+    Generating WoT resources (RDF, JSON-LD Context, SHACL Shapes, and JSON Schema)
+    and the final Respec specification from LinkML schemas.
     """
-    if input_schema and not Path(input_schema).exists():
+    input_path = Path(input_schema)
+    if not input_path.exists():
         raise FileNotFoundError(f"Cannot find input LinkML schema file {input_schema}.")
-    elif not input_schema and not YAML_SCHEMA_PATH.exists():
-        raise FileNotFoundError(f"Cannot find the default LinkML schema file {YAML_SCHEMA_PATH}.")
-    else:
-        try:
-            linkml_schema_view = SchemaView(input_schema, merge_imports=True)
-            logging.info(f"Input schema {input_schema} loaded successfully!")
-            for generator in GENERATORS:
-                output_dir = GENS_PATH / generator
-                output_dir.mkdir(parents=True, exist_ok=True)
-                logging.info(f"Proceeding with WoT resource generation")
-                run_generator(linkml_schema_view, generator, output_dir)
-        except yaml.YAMLError as e:
-            logging.info(f"LinkML schema validation failed: {e}")
-        if generate_docs:
-            logging.info(f"Generating documentation locally as markdown files...")
-            generate_documentation()
-        if serve_docs:
-            logging.info(f"Serving documentation...")
-            serve_documentation()
+
+    try:
+        linkml_schema_view = SchemaView(input_path, merge_imports=True)
+        logging.info(f"Input schema {input_schema} loaded successfully!")
+
+        for generator in GENERATORS:
+            output_dir = GENS_PATH / generator
+            output_dir.mkdir(parents=True, exist_ok=True)
+            logging.info(f"Proceeding with WoT resource generation for {generator}")
+            run_generator(linkml_schema_view, generator, output_dir)
+
+        if generate_spec:
+            generate_respec_spec(input_path, GENS_PATH)
+
+    except yaml.YAMLError as e:
+        logging.error(f"LinkML schema validation failed: {e}")
 
 
 if __name__ == "__main__":
